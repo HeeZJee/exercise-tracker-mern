c894ce251e819170bea556c613aa8c47c1123599
Hafeez Ghanchi <hafeezghanchi927@gmail.com>
2021-02-27 17:25:47 +0500

    created updated and delete exercise api

--- backend/routers/exercises.ts
+++ backend/routers/exercises.ts
@@ -20,4 +20,32 @@ router.route("/add").post((req: any, res: any, next: any) => {
     .catch((err: any) => next(err));
 });
 
+router.route("/:id").get((req: any, res: any, next: any) => {
+  Exercise.findById(req.params.id)
+    .then((exercise: any) => res.json(exercise))
+    .catch((err: any) => next(err));
+});
+
+router.route("/:id").delete((req: any, res: any, next: any) => {
+  Exercise.findByIdAndDelete(req.params.id)
+    .then(() => res.json("Exercise deleted"))
+    .catch((err: any) => next(err));
+});
+
+router.route("/update/:id").post((req: any, res: any, next: any) => {
+  Exercise.findById(req.params.id)
+    .then((exercise: any) => {
+      exercise.username = req.body.username;
+      exercise.description = req.body.description;
+      exercise.duration = Number(req.body.duration);
+      exercise.date = new Date();
+
+      exercise
+        .save()
+        .then(() => res.json("Exercise updated"))
+        .catch((err: any) => next(err));
+    })
+    .catch((err: any) => next(err));
+});
+
 module.exports = router;
