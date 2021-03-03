261528015de5644a9a965799adeb6e2e429b2ff4
Hafeez Ghanchi <hafeezghanchi927@gmail.com>
2021-03-03 06:07:22 +0500

    create user and exercise logic

--- src/components/CreateUser.tsx
+++ src/components/CreateUser.tsx
@@ -1,9 +1,61 @@
-import React, { ReactElement } from "react";
+import React, { Component } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
-interface Props {}
+export default class CreateExercise extends Component<any, any> {
+  constructor(props: any) {
+    super(props);
 
-function CreateUser({}: Props): ReactElement {
-  return <div>Create User</div>;
+    this.onChangeUsername = this.onChangeUsername.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+
+    this.state = {
+      username: "",
+    };
+  }
+
+  onChangeUsername(e) {
+    this.setState({
+      username: e.target.value,
+    });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+    const newUser = {
+      username: this.state.username,
+    };
+    console.log(newUser);
+
+    this.setState({
+      username: "",
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>Create New User</h3>
+        <form onSubmit={this.onSubmit}>
+          <div className="form-group">
+            <label>Username: </label>
+            <input
+              type="text"
+              required
+              className="form-control"
+              value={this.state.username}
+              onChange={this.onChangeUsername}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="submit"
+              value="Create User"
+              className="btn btn-primary"
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
-
-export default CreateUser;
---
+++ src/utils/types.tsx
@@ -0,0 +1,7 @@
+export interface iExercise {
+  username: string;
+  description: string;
+  duration: number;
+  date: Date;
+  users: Array<string>;
+}
